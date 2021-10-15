85c0cdbb370a88467abbbad52eb0e489e5992f9c
Dimuth Kurukula <dkurukul@uwo.ca>
2021-10-14 23:17:10 -0400

    start app outline

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -8,9 +8,9 @@ import { RootTabScreenProps } from '../types';
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Potential homes</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Text style={styles.body}>My catalogue</Text>
     </View>
   );
 }
@@ -24,6 +24,9 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+  },
+  body: {
+    fontSize: 16,
   },
   separator: {
     marginVertical: 30,
