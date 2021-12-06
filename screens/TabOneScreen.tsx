ab30e371e1bcdad58c245b75b1b279edba829419
Dimuth Kurukula <dkurukul@uwo.ca>
2021-12-05 19:09:55 -0500

    Start cost of ownership calc
    Use react native elements as UI kit

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,16 +1,47 @@
 import * as React from 'react';
+import { useState} from 'react';
 import { StyleSheet } from 'react-native';
+import {Input} from 'react-native-elements'
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+interface listing {
+  purchasePrice: number,
+  propertyTax: number,
+  maintFee: number,
+  sqrft:number,
+  notIncludedExpense: number,
+  monthlyCostOfOwnership:number
+}
+
+const initialListing:listing = {
+  purchasePrice: 0,
+  propertyTax: 0,
+  maintFee: 0,
+  sqrft: 0,
+  notIncludedExpense:0,
+  monthlyCostOfOwnership:0
+}
+
+const TabOneScreen = ({ navigation }: RootTabScreenProps<'TabOne'>) => {
+  const [listing, setListing] = useState(initialListing)
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Potential homes</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <Text style={styles.body}>My catalogue</Text>
+      <Text>{JSON.stringify(listing)}</Text>
+      <Input
+        placeholder="Purchase Price"
+    onChangeText={value=> setListing({...listing, purchasePrice: +value})}
+
+      />
+      <Input
+        placeholder="Square Footage"
+    onChangeText={value => setListing({...listing, sqrft: +value})}
+      />
     </View>
   );
 }
@@ -34,3 +65,5 @@ const styles = StyleSheet.create({
     width: '80%',
   },
 });
+
+export default TabOneScreen;
