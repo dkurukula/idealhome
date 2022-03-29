c85442efb3efae5ddb124f29e74556d13c428185
Dimuth Kurukula <dkurukul@uwo.ca>
2022-03-29 00:57:40 -0400

    feat: mortage input ui

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -4,6 +4,7 @@ import { StyleSheet, ScrollView } from "react-native";
 import { format } from "d3-format";
 import { Text, View, Input } from "../components/Themed";
 import { RootTabScreenProps } from "../types";
+import { CheckBox } from "react-native-elements";
 
 interface listing {
   purchasePrice: number;
@@ -14,6 +15,8 @@ interface listing {
   monthlyCostOfOwnership: number;
   downPaymentPercent: number;
   downPaymentAmount: number;
+  interestRate: number;
+  schedule: "monthly" | "bi-weekly" | "bi-weekly accelerated";
 }
 
 const initialListing: listing = {
@@ -24,7 +27,9 @@ const initialListing: listing = {
   notIncludedExpense: 0,
   monthlyCostOfOwnership: 0,
   downPaymentPercent: 20,
-  downPaymentAmount: 0
+  downPaymentAmount: 0,
+  interestRate: 2.2,
+  schedule: "bi-weekly"
 };
 
 interface resultType {
@@ -103,6 +108,68 @@ const TabOneScreen = ({ navigation }: RootTabScreenProps<"TabOne">) => {
           </View>
         </View>
 
+        <View style={styles.flexContainer}>
+          <View style={styles.interestContainer}>
+            <View
+              style={{
+                flex: 1,
+                padding: 5,
+                minWidth: 30
+              }}
+            >
+              <Input
+                label="Interest Rate"
+                placeholder="2.2%"
+                onChangeText={value =>
+                  value && setListing({ ...listing, interestRate: +value })
+                }
+                darkColor="#eee"
+                lightColor="rgba(255,255,255,0.1)"
+              />
+            </View>
+            <View
+              style={{
+                flex: 3,
+                flexDirection: "row",
+                flexWrap: "wrap"
+              }}
+            >
+              <View style={{ flex: 3, minWidth: 100 }}>
+                <CheckBox
+                  title="Monthly"
+                  checked={listing.schedule === "monthly" ? true : false}
+                  onPress={() =>
+                    setListing({ ...listing, schedule: "monthly" })
+                  }
+                />
+              </View>
+              <View style={{ flex: 3, minWidth: 110 }}>
+                <CheckBox
+                  title="Bi-weekly"
+                  checked={listing.schedule === "bi-weekly" ? true : false}
+                  onPress={() =>
+                    setListing({ ...listing, schedule: "bi-weekly" })
+                  }
+                />
+              </View>
+              <View style={{ flex: 4, minWidth: 140 }}>
+                <CheckBox
+                  title="Bi-weekly accel."
+                  checked={
+                    listing.schedule === "bi-weekly accelerated" ? true : false
+                  }
+                  onPress={() =>
+                    setListing({
+                      ...listing,
+                      schedule: "bi-weekly accelerated"
+                    })
+                  }
+                />
+              </View>
+            </View>
+          </View>
+        </View>
+
         <View style={styles.resultGroupContainer}>
           <Result
             label="Price per Sqft"
@@ -159,6 +226,16 @@ const styles = StyleSheet.create({
   result: { fontSize: 20, paddingHorizontal: 5 },
   flexContainer: { flex: 1, padding: 10 },
   row: { flexDirection: "row", flexWrap: "wrap" },
+  interestContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    flex: 1,
+    width: "100%",
+    paddingVertical: 10,
+    borderWidth: 1,
+    borderStyle: "dotted",
+    borderColor: "grey"
+  },
   separator: {
     marginVertical: 30,
     height: 1,
